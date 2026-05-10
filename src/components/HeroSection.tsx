33831465c4411205dd32d773bd69fb76f1269f7e
Rahul Raghunathan <rrahul2k1@gmail.com>
2026-05-10 22:07:18 +0530

    nav bar & landing page

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className="max-w-3xl">
+        <div></div>
+
+    </section>
+  )
+}
+
+export default HeroSection
